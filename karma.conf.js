4ae32aedb3a6671c68a6b6ac9b38df246d1ba00c
Ivan Dmitriev <hiskind@gmail.com>
2017-04-25 23:29:25 +0400

    Turn on source map support for tests

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
         'test/*.js': ['browserify']
     },
     browserify: {
+        debug: true,
         configure: function(bundle) {
             bundle.once('prebundle', function() {
                 bundle.transform('babelify', {
